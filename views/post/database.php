671b8dc1bb866d40bb6df314360dbffd1f0dd67b
edwinchu0711 <162130385+edwinchu0711@users.noreply.github.com>
2025-04-12 20:50:45 +0800

    Add files via upload

--- views/post/database.php
+++ views/post/database.php
@@ -1,14 +1,4 @@
-<script>
-// 檢測連線錯誤並自動修復
-alert("hello");
-window.onerror = function(msg) {
-    if (msg.includes('driver') || msg.includes('連接失敗')) {
-        console.log('檢測到資料庫錯誤，正在重新整理...');
-        location.reload(true);
-        return true;
-    }
-};
-</script>
+
 
 <?php
 /**
